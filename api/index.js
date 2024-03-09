af6bc9169913a36e9ed80c9152d1f04620c62424
Jake Ma4o <2000_aktau@mail.ru>
2024-03-09 15:00:34 +0500

    Added mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,34 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+
+dotenv.config()
 
 const app = express();
+app.use(express.json())
+
+// const connectionString = process.env.MONGODB_URI || "";
+const connectionString = process.env.MONGODB_URI || "";
+
+const itemSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestaps: true
+  }
+)
+
+const Item = mongoose.model("Item", itemSchema);
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to express hosted on vercel")
@@ -11,8 +38,25 @@ app.get("/po", (req, res) => {
   res.send("Po?")
 })
 
-app.listen(3000 || process.env.PORT, () => {
-  console.log("Server is listenning on port 3000")
+app.get("/items", async (req, res) => {
+  try {
+    const items = await Item.find({})
+    res.status(200).json(items);
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+
+mongoose.connect(connectionString)
+.then(() => {
+  console.log("App is connected to database")
+  app.listen(3000 || process.env.PORT, () => {
+    console.log("Server is listenning on port 3000")
+  })
+})
+.catch((error) => {
+  console.log(error)
 })
 
 
